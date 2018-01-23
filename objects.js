2e6b11cbb24d92fa1852a6fe5469860e8f019e39
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-23 22:39:29 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist ={ladyGaga: "Pocker Face", selenaGomez: "Who says", frankSinatra: "New York"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
